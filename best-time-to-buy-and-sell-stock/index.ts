5fab60f562e61f7a7a128f12274d2e58f8fbfe89
FuJuntao <fu.juntao358@gmail.com>
2021-03-01 11:35:18 +0800

    add solution

--- best-time-to-buy-and-sell-stock/index.ts
+++ best-time-to-buy-and-sell-stock/index.ts
@@ -1,4 +1,13 @@
-function maxProfit(prices: number[]): number {}
+function maxProfit(prices: number[]): number {
+  let maxProfit = 0;
+  let minPrice = prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    // 第 i 天卖出获得的最大利润 = price[i] - min(price[0] ~ price[i-1])
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    minPrice = Math.min(minPrice, prices[i]);
+  }
+  return maxProfit;
+}
 
 // 暴力解法
 // function maxProfit(prices: number[]): number {
