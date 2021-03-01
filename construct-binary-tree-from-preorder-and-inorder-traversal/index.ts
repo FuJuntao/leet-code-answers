0d9a816c02fc0193ef3fb08101e96aaa6f959b0b
FuJuntao <fu.juntao358@gmail.com>
2021-03-01 11:35:12 +0800

    fail test

--- construct-binary-tree-from-preorder-and-inorder-traversal/index.ts
+++ construct-binary-tree-from-preorder-and-inorder-traversal/index.ts
@@ -1,5 +1,7 @@
 import { TreeNode } from "../TreeNode.ts";
 
-function buildTree(preorder: number[], inorder: number[]): TreeNode | null {}
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+  return new TreeNode();
+}
 
 export { buildTree };
--- median-of-two-sorted-arrays/index.ts
+++ median-of-two-sorted-arrays/index.ts
@@ -2,31 +2,32 @@ const toInt = (n: number) => Math.floor(n);
 
 export function findMedianSortedArrays(
   nums1: number[],
-  nums2: number[],
+  nums2: number[]
 ): number {
-  const nums1Length = nums1.length;
-  const nums2Length = nums2.length;
+  // const nums1Length = nums1.length;
+  // const nums2Length = nums2.length;
 
-  let leftEdge = 0;
-  let rightEdge = nums1Length;
+  // let leftEdge = 0;
+  // let rightEdge = nums1Length;
 
-  while (leftEdge <= rightEdge) {
-    const i = toInt((rightEdge - leftEdge) / 2);
-    const j = toInt((nums1Length + nums2Length) / 2) - i;
+  // while (leftEdge <= rightEdge) {
+  //   const i = toInt((rightEdge - leftEdge) / 2);
+  //   const j = toInt((nums1Length + nums2Length) / 2) - i;
 
-    const left1 = nums1[i];
-    const left2 = nums2[j];
-    const right1 = nums1[i + 1];
-    const right2 = nums2[j + 1];
+  //   const left1 = nums1[i];
+  //   const left2 = nums2[j];
+  //   const right1 = nums1[i + 1];
+  //   const right2 = nums2[j + 1];
 
-    if (left1 <= right2) {
-      leftEdge = i + 1;
-    } else {
-      rightEdge = i - 1;
-    }
-  }
+  //   if (left1 <= right2) {
+  //     leftEdge = i + 1;
+  //   } else {
+  //     rightEdge = i - 1;
+  //   }
+  // }
 
-  return leftEdge;
+  // return leftEdge;
+  return 0;
 }
 
 const nums1 = [1, 2, 3, 4, 5, 6, 7, 18, 20, 23, 24];
